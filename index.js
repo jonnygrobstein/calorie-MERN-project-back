efdc9eda4382415ca1700217a4055a9426105319
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-23 21:19:16 -0500

    moved user router to routes folder

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import { passportFunc } from "./config/passport.js";
-import userController from "./controllers/users.js";
 import express from "express";
+import router from "./routes/index.js";
+
 const app = express();
-/* import router from "./routes/index.js"; */
-
 const passport = passportFunc();
+
 app.use(express.json());
-
 app.use(passport.initialize());
-app.use("/users", userController);
-
-/* app.use(router); */
+app.use("/", router);
 
 app.listen(4000, () => console.log("I hear you sneaking around on Port 4000"));
 //changed to 4000 from 3000
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import foodRouter from "./Food.js";
 import mealRouter from "./Meals.js";
+import userController from "../controllers/users.js";
 
 const router = express.Router();
 
 router.use("/foods", foodRouter);
 
 router.use("/meals", mealRouter);
+
+router.use("/users", userController);
 
 export default router;
