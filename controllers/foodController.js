549d0b71b1e79f1dc6cea23fe6d69cf2131a92fb
Papa Bear's Handler <117048521+jonnygrobstein@users.noreply.github.com>
2023-03-25 16:03:38 -0500

    Update foodController.js

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -2,10 +2,29 @@ import Food from "../models/Food.js";
 
 const foodController = {
   index: (req, res) => {
-    Food.find({}).then((food) => {
-      res.json(food);
+    Food.find({})
+      .then((food) => {
+        res.json(food);
     });
   },
+  create: async(req, res) => {
+    console.log(req.body)
+    const food = await Food.create(req.body)
+      res.json(food)
+      
+  },
+  edit: (req, res) => {
+    Food.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
+      .then(food => {
+        res.json(food)
+      })
+  },
+  delete: (req, res) => {
+    Food.findOneAndRemove({_id: req.params.id})
+      .then(food => {
+        res.json(food)
+      })
+  }
 };
 
 export default foodController;
