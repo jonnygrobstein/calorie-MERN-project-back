8f932f534a5ae8a64fbbb1dba48a208c23fa0701
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-03-25 16:16:06 -0500

    Food routes

--- routes/Food.js
+++ routes/Food.js
@@ -4,5 +4,8 @@ import foodController from "../controllers/foodController.js";
 const foodRouter = express.Router();
 
 foodRouter.get("/", foodController.index);
+foodRouter.post('/addFood', foodRouter.create)
+foodRouter.put('/:id', foodRouter.edit)
+foodRouter.delete('/:id', foodRouter.delete)
 
 export default foodRouter;
