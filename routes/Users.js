ecdf6b9b840ab74abc8313bca838dd309b917eec
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-26 16:48:15 -0500

    setup login route

--- routes/Users.js
+++ routes/Users.js
@@ -4,5 +4,6 @@ import usersController from "../controllers/usersController.js";
 const userRouter = express.Router();
 
 userRouter.post("/signup", usersController.signup);
+userRouter.post("/login", usersController.login);
 
 export default userRouter;
