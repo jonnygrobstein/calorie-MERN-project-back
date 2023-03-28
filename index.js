5a7c977fc5a557dfeb13e5aaf3b0262819dd8a0e
Jonny Grobstein <jonnygrobstein@gmail.com>
2023-03-27 20:16:12 -0500

    cors add

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { passportFunc } from "./config/passport.js";
 import express from "express";
 import router from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 const passport = passportFunc();
 
 app.use(express.json());
 app.use(passport.initialize());
+app.use(cors());
 
 app.use("/", router);
 
