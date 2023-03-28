171c19b7c59983689d6deac55ecb720b0c9944ca
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-27 22:09:18 -0500

    find food controller now works as a keyword

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -2,38 +2,40 @@ import Food from "../models/Food.js";
 
 const foodController = {
   index: (req, res) => {
-    Food.find({})
-      .then((food) => {
-        res.json(food);
+    Food.find({}).then((food) => {
+      res.json(food);
     });
   },
   findFood: (req, res) => {
-    Food.find({food_name: req.query.food})
-    .then((food) => {
-      res.json(food)
-    })
-    .catch((error) => {
-      console.log(error)
-    })
+    const searchedFood = new RegExp(".*" + req.query.food + ".*");
+
+    Food.find({ food_name: searchedFood })
+      .then((food) => {
+        res.json(food);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
-  create: async(req, res) => {
-    console.log(req.body)
-    const food = await Food.create(req.body)
-      res.json(food)
-      
+  create: async (req, res) => {
+    console.log(req.body);
+    const food = await Food.create(req.body);
+    res.json(food);
   },
   edit: (req, res) => {
-    Food.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
-      .then(food => {
-        res.json(food)
-      })
+    Food.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    ).then((food) => {
+      res.json(food);
+    });
   },
   delete: (req, res) => {
-    Food.findOneAndRemove({_id: req.params.id})
-      .then(food => {
-        res.json(food)
-      })
-  }
+    Food.findOneAndRemove({ _id: req.params.id }).then((food) => {
+      res.json(food);
+    });
+  },
 };
 
 export default foodController;
