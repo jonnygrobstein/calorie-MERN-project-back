42052e4755646c172117830d5a22424a03b46ca0
Jose Padilla <jose.apadilla99@gmail.com>
2023-03-28 20:58:21 -0500

    debugged register- schema was changed

--- models/User.js
+++ models/User.js
@@ -1,17 +1,20 @@
 import mongoose from "../db/connection.js";
-import Meals from "./MealPlans"
+import Meals from "./MealPlans.js"
 
 const Schema = mongoose.Schema;
 
 const User = new Schema({
     username: { type: String, required: true, unique: true },
-    user_email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type:String, required: true},
     user_firstname: { type: String, required: true },
     user_lastname: { type: String, required: true },
     user_city: String,
     user_state_province: String,
     user_country: String,
     user_mealPlans: [{ type: Schema.Types.ObjectId, ref: `Meals` }]  
+    //login works, but username, firstname, and lastname need to be commented out
+    //until frontend has these options
 })
 
 
