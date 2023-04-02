76691a372444580b3c9e2acbba39614d13f87d22
Jose Padilla <jose.apadilla99@gmail.com>
2023-04-02 00:14:30 -0500

    setup mongodb atlas

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb://localhost/calories', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(/* 'mongodb://localhost/calories' */ "mongodb+srv://joseapadilla99:VVavFrSvRfZLRROd@cluster0.zrt4jyc.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+//swap for local and deployment
 
 export default mongoose
